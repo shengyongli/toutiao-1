d864597c77c61cefdea0fb574a7a7a0db3c199fc
盛永丽 <1365125994@qq.com>
2019-09-11 11:11:50 +0800

    文件目录初始整理

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' // 引入UI
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './style/index.less' // 引入初始化样式 包括margin padding li 默认样式的清除
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
